813c57d149a79be3bfea1445670ab28f9659fe16
ia <paepulumbarit@gmail.com>
2026-02-10 10:17:02 -0800

    Set token expiration

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Response;
 use Laravel\Socialite\Facades\Socialite;
-use Log;
 
 class GoogleAuthController extends Controller
 {
@@ -18,14 +18,17 @@ class GoogleAuthController extends Controller
     public function callback()
     {
         $googleUser = Socialite::driver('google')->stateless()->user();
-        Log::info('Google ID: '.$googleUser->getId());
 
         $user = User::updateOrCreate(['email' => $googleUser->getEmail()], [
             'name' => $googleUser->getName(),
             'google_id' => $googleUser->getId(),
         ]);
 
-        $token = $user->createToken('api')->plainTextToken;
+        $token = $user->createToken(
+            'api',
+            ['*'],
+            Carbon::now()->addDays(4)
+        )->plainTextToken;
 
         return Response::json([
             'token' => $token,
